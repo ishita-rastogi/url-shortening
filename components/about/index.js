3c806f8b55bb505f2ba802395e024c720cc24853
ishita-rastogi <66439654+ishita-rastogi@users.noreply.github.com>
2024-03-20 16:52:03 -0700

    Create index.js

---
+++ components/about/index.js
@@ -0,0 +1,14 @@
+import style from "./about.module.css";
+
+export default function About() {
+	return (
+		<div className={style.about}>
+			<a
+				href="https://ishita-rastogi.github.io/url-shortening/"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+			</a>
+		</div>
+	);
+}
